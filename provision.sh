24b0321dea31da570fe36eaa8721fb0db413e989
TheYOSH <theyosh@theyosh.nl>
2019-10-05 11:55:27 +0200

    update installation

--- provision.sh
+++ provision.sh
@@ -5,11 +5,14 @@ apt-get -y update && apt-get -y upgrade && apt-get -y install git nginx php-fpm 
 usermod -G vagrant -a www-data
 usermod -G www-data -a vagrant
 
-rm /etc/nginx/sites-enabled/default
-ln -s /home/vagrant/dreamboxrestream/nginx.conf /etc/nginx/sites-enabled/dreamboxrestream
+cd /home/vagrant/dreamboxrestream/
+
+rm /etc/nginx/sites-enabled/default || true
+su -c 'cp nginx.conf.example nginx.conf' vagrant
+sed -i -e 's@root .*@root '`pwd`'/public;@g' nginx.conf
+ln -s `pwd`/nginx.conf /etc/nginx/sites-enabled/dreambox_restream.conf
 service nginx restart
 
-cd /home/vagrant/dreamboxrestream/
 
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -31,11 +34,10 @@ su -c 'php composer.phar install' vagrant
 su -c 'touch database/restream.sqlite' vagrant
 su -c 'cp .env.example .env' vagrant
 
-sed -i -e 's/APP_NAME=.*/APP_NAME=DreamboxReStream/g' /home/vagrant/dreamboxrestream/.env
-sed -i -e 's/APP_DEBUG=.*/APP_DEBUG=false/g' /home/vagrant/dreamboxrestream/.env
-
-sed -i -e 's/DB_CONNECTION=.*/DB_CONNECTION=sqlite/g' /home/vagrant/dreamboxrestream/.env
-sed -i -e 's/DB_DATABASE=.*/DB_DATABASE=\/home\/vagrant\/dreamboxrestream\/database\/restream\.sqlite/g' /home/vagrant/dreamboxrestream/.env
+sed -i -e 's@APP_NAME=.*@APP_NAME=DreamboxReStream@g' .env
+sed -i -e 's@APP_DEBUG=.*@APP_DEBUG=false@g' .env
+sed -i -e 's@DB_CONNECTION=.*@DB_CONNECTION=sqlite@g' .env
+sed -i -e 's@DB_DATABASE=.*@DB_DATABASE='`pwd`'/database/restream\.sqlite@g' .env
 
 su -c 'php artisan key:generate' vagrant
 su -c 'php artisan migrate' vagrant
