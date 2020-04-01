54f2e14acde8772a60b9d4df39350a8ddff60047
theyosh <theyosh@theyosh.nl>
2020-04-01 20:56:59 +0200

    Speed up Sqlite about 100 times!!! YEAH!!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Don't kill the app if the database hasn't been created.
+        // https://www.whoishostingthis.com/compare/sqlite/optimize/
+        // https://medium.com/swlh/laravel-optimizing-sqlite-to-dangerous-speeds-ff04111b1f22
+        try {
+            DB::connection('sqlite')->statement(
+                'PRAGMA synchronous = OFF;'
+            );
+            DB::connection('sqlite')->statement(
+                'PRAGMA journal_mode = MEMORY;'
+            );
+            DB::connection('sqlite')->statement(
+                'PRAGMA temp_store = MEMORY;'
+            );
+        } catch (\Throwable $throwable) {
+            return;
+        }
     }
 }
