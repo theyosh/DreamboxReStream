c16e8154cf2bbdcc39c5c8ae19ad468abf3f4d72
TheYOSH <github.com@theyosh.nl>
2022-05-23 16:59:26 +0200

    Update build scripts

--- vm_ip.sh
+++ vm_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 3
+sleep 5
 DATE="May 23th, 2022"
 IP=`ip addr | grep inet | sed 's/[ ]\+//' | cut -d" " -f 2 | grep -v 127.0.0 | grep -v ::1/128 | grep -v fe80 | sed 's@/.*@@'`
 HOST=`hostname`
